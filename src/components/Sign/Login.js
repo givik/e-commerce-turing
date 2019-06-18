1ed42069267d0ccffefee694e2079f9b0193cd15
Givi Kuchukhidze <givi.kuchukhidze@gmail.com>
2019-06-18 05:12:17 +0400

    login, registration markup

--- src/components/Sign/Login.js
+++ src/components/Sign/Login.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Login = () => {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <div>
+        <input type="text" placeholder="login" />
+      </div>
+      <div>
+        <input type="password" placeholder="password" />
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Sign/Registration.js
+++ src/components/Sign/Registration.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const Registration = () => {
-  return <div className="registration">Registration</div>;
+  return (
+    <div className="registration">
+      <h1>Register</h1>
+      <div>
+        <input type="text" placeholder="login" />
+      </div>
+      <div>
+        <input type="password" placeholder="password" />
+      </div>
+      <div>
+        <input type="password" placeholder="repeat password" />
+      </div>
+      <div>
+        <button>Registration</button>
+      </div>
+    </div>
+  );
 };
 
 export default Registration;
