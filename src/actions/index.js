94456249a34adbec92741af6ef28d9f94f384cba
Givi Kuchukhidze <givi.kuchukhidze@gmail.com>
2019-06-19 18:57:38 +0400

    auth 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,25 @@ import history from '../history';
 import { SIGN_IN, SIGN_OUT } from './types';
 
 export const customerPostFetch = formValues => async dispatch => {
-  console.log(formValues);
-  const response = ecomerce.post('/customer', formValues);
-  // console.log(response);
+  ecomerce
+    .post('/customers', formValues)
+    .then(response => {
+      alert('You have successfully registered.');
+      localStorage.setItem('token', response.data.accessToken);
+      dispatch(signUser(response.data));
+      history.push('/');
+    })
+    .catch(error => {
+      alert(error.response.data.error.message);
+    });
 };
+
+export const signUser = user => ({
+  type: SIGN_IN,
+  payload: user
+});
+
+export const signOutUser = user => ({
+  type: SIGN_OUT,
+  payload: user
+});
